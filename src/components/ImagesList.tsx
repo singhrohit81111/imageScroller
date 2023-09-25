c8a9557462bbb918ac2e9af8dec564f2ef310e09
Rohit Singh <singhrohit8111@gmail.com>
2023-09-25 17:40:04 +0530

    added ref to the the observer

--- src/components/ImagesList.tsx
+++ src/components/ImagesList.tsx
@@ -10,61 +10,57 @@ export default function ImagesList() {
     const imageRef = useRef<HTMLDivElement | null>(null);
     const [initalCount, setInitialCount] = useState<number>(0);
     const [error,setError]=useState<boolean>(false);
+    const observerRef = useRef<IntersectionObserver | null>(null);
 
     console.log(imageRef);
 
-    // laodMore();
+    const laodMore = () => {
+            
+        axios.get(`https://api.slingacademy.com/v1/sample-data/photos?offset=${initalCount}&limit=20`).then(res => {
+            console.log(res.status);
+            console.log(window.navigator.onLine);
+                const nextImages = res.data.photos.map((photo: any) => {
+                    return photo.url;
+
+                })
+                if (nextImages.length === 0) {
+                    setIsLoading(false);
+                    observerRef.current!.disconnect();
+                    return;
+                }
+                console.log(nextImages);
+                setVisibleImages(prevImages => { return [...prevImages, ...nextImages] });
+                setInitialCount(initalCount + 20);
+            
+        })
+    }
+
     useEffect(() => {
         
-        const laodMore = () => {
-            
-            axios.get(`https://api.slingacademy.com/v1/sample-data/photos?offset=${initalCount}&limit=20`).then(res => {
-                console.log(res.status);
-                console.log(window.navigator.onLine);
-                    const nextImages = res.data.photos.map((photo: any) => {
-                        return photo.url;
-
-                    })
-                    if (nextImages.length === 0) {
-                        setIsLoading(false);
-                        observer.disconnect();
-                        return;
-                    }
-                    console.log(nextImages);
-                    setVisibleImages(prevImages => { return [...prevImages, ...nextImages] });
-                    setInitialCount(initalCount + 20);
-                
-            })
-        }
-
-        const observer = new IntersectionObserver((entries) => {
+        observerRef.current = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     console.log(entry);
-                    if(navigator.onLine){
-                        laodMore();
-                        console.log(window.navigator.onLine);
-                        
-                    }
-                    else if(!navigator.onLine){
+                     if(!navigator.onLine){
                         console.log("hfghjfhhtftyg",navigator.onLine);
-                        window.alert("ERROR");
-                        
+                        window.alert("adadwqewaas");
                         
                     }
+                    laodMore();
+                    console.log(navigator.onLine);
                    
                 }
             });
         }, { threshold: 0.25 });
 
         if (imageRef.current) {
-            observer.observe(imageRef.current);
+            observerRef.current.observe(imageRef.current);
         }
 
         return () => {
-            observer.disconnect();
+            observerRef.current?.disconnect?.();
         };
-    }, [initalCount,navigator.onLine])
+    }, [initalCount,window.navigator.onLine])
     return (
         <div className='images'>
             {visibleImages.map(visibleImage => {
